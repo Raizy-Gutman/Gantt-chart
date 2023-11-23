a46076ff44789e9e486a0bc06f42b46f0a494c48
Raizy-Gutman <rg4183126@gmail.com>
2023-11-23 10:54:12 +0200

    Chapter 8 Initialization of a database in values, in the DalTest project, as well as the beginning of Chapter 9

--- DalTest/Initialization.cs
+++ DalTest/Initialization.cs
@@ -2,6 +2,8 @@
 using DalApi;
 using DO;
 using System.ComponentModel;
+using System.Security.Cryptography;
+using System.Xml.Linq;
 
 
 public static class Initialization
@@ -10,19 +12,25 @@ public static class Initialization
     private static IDependency? s_dalDependency;
     private static ITask? s_dalTask;
 
+    public static void Do(IEngineer? dalEngineer, IDependency? dalDependency, ITask? dalTask)
+    {
+        s_dalEngineer = dalEngineer ?? throw new NullReferenceException("DAL can not be null!");
+        s_dalDependency = dalDependency ?? throw new NullReferenceException("DAL can not be null!");
+        s_dalTask = dalTask ?? throw new NullReferenceException("DAL can not be null!");
+        craeteEngineers();
+        craeteTask();
+        craeteDependency();
+    }
+
     private static readonly Random s_rand = new();
     private static void craeteEngineers()
     {//לפחות 5 מהנדסים
 
-    
-
-    string[] engineerNames =
+        string[] engineerNames =
         {
             "Raizy Gutman", "Yeudit Itamar", "Dani Levi", "Eli Amar", "Yair Cohen",
             "Ariela Levin", "Dina Klein", "Shira Israelof"
         };
-
-
 
         foreach (var _name in engineerNames)
         {
@@ -32,33 +40,54 @@ public static class Initialization
             while (s_dalEngineer!.Read(_id) != null);
 
             string? _email = string.Join("", _name.Split(default(string[]), StringSplitOptions.RemoveEmptyEntries)) + "@gmail.com";
-            double? _cost = s_rand.Next(5000, 15000);  
+            double? _cost = s_rand.Next(5000, 15000);
             EngineerExperience _level = (EngineerExperience)s_rand.Next(Enum.GetNames(typeof(EngineerExperience)).Length);
             Engineer newEngineer = new(_id, _name, _email, _level, _cost);
             s_dalEngineer!.Create(newEngineer);
-
         }
     }
 
-    //private static craeteTask()
-    //{
-    //    
+    private static void craeteTask()
+    {//לפחות 20 משימות
 
-    //}
+        for (int i = 0; i < 30; i++)
+        {
+            int _id = 0;
+            string? _Description = "this is the " + i.ToString()+ "task.";
+            string? _alias = "T" + i.ToString();
+            bool _isMilestone = false;
+            int days = s_rand.Next(1, 28);
+            int month = s_rand.Next(1, 12);
+            int year = s_rand.Next(2021, 2023);
+            DateTime? _createdAtDate = new DateTime(year, month, days);
+            DateTime? _startDate = null;
+            DateTime? _schedualDate = null;
+            DateTime? _forecastDate = null;
+            DateTime? _deadlineDate = null;
+            DateTime? _completeDate = null;
+            string? _deliverables = "Deliverable of the " + i.ToString() + "task.";
+            string? _remarks = null;
+            int _engineerId = 0;//ללא הקצאת מהנדס?!
+            EngineerExperience _complexityLevel = (EngineerExperience)s_rand.Next(Enum.GetNames(typeof(EngineerExperience)).Length);
+            Task newTask = new(_id, _Description, _alias, _isMilestone, _createdAtDate, _startDate, _schedualDate, _forecastDate, _deadlineDate, _completeDate, _deliverables, _remarks, _engineerId, _complexityLevel);
+            s_dalTask!.Create(newTask);
+        }
+    }
 
 
-    //private static void craeteDependency()
-    //{
+    private static void craeteDependency()
+    {//לפחות 40 תלויות
 
-    //    int Id,
-    //    int DependentTask,
-    //    int DependsOnTask
-    //    לפחות 20 משימות
-    //}
-
-
-
-
-
-
+        int _id = 0;// ללא הקצאת ID??
+        for(int i = 0; i < 30; i++)
+        {
+            int _dependentTask = i;
+            for (int j = 0; j < i; j++)
+            {
+                int DependsOnTask = j;
+                Dependency newDependency = new(_id, _dependentTask, DependsOnTask);
+                s_dalDependency!.Create(newDependency);
+            }
+        }
+    }
 }
---
+++ DalTest/Program.cs
@@ -0,0 +1,57 @@
+﻿using Dal;
+using DalApi;
+
+namespace DalTest
+{
+    internal class Program
+    {
+        private static IEngineer? s_dalEngineer = new EngineerImplementation();
+        private static IDependency? s_dalDependency = new DependencyImplementation();
+        private static ITask? s_dalTask = new TaskImplementation();
+        static void Main(string[] args)
+        {
+            try
+            {
+                Initialization.Do(s_dalEngineer, s_dalDependency, s_dalTask);
+                //Console.WriteLine("Hello, World!");
+
+                while (true)
+                {
+                    DisplayMainMenu();
+                    string userChoice = Console.ReadLine();
+
+                    if (userChoice == "0")
+                    {
+                        break; // Exit the program
+                    }
+
+                    ProcessUserChoice(userChoice);//צריך לממש בחירת משתמש
+                }
+
+
+                private static void DisplayMainMenu()
+                {
+                    Console.WriteLine("Main Menu:");
+                    Console.WriteLine("1. Entity Engineer");
+                    Console.WriteLine("2. Entity Dependency");
+                    Console.WriteLine("3. Entity Task");
+                    Console.WriteLine("0. Exit");
+                    Console.Write("Enter your choice: ");
+                }
+
+                
+            }
+            catch (Exception ex) 
+            {//או הראשון או השני
+                //Console.WriteLine($"Initialization failed. Exception: {ex.Message}");
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
+
+
+    
+        
+    
+
