d63103a124061e0e0128450c61ada1bbe6edf985
Raizy-Gutman <rg4183126@gmail.com>
2024-02-12 18:00:56 +0200

    the beginning of chapter 6 in step 4

---
+++ BL/BO/Task.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO
+{
+    public class Task
+    {
+        public int Id { get; init; }
+        public string? Description { get; set; }
+        public string? Alias { get; set; }
+        public bool IsMilestone { get; set; }
+        public DateTime? CreatedAtDate { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? SchedualDate { get; set; }
+        public TimeSpan? Duration { get; set; }
+        public DateTime? DeadlineDate { get; set; }
+        public DateTime? CompleteDate { get; set; }
+        public string? Deliverables { get; set; }
+        public string? Remarks { get; set; }
+        public int EngineerId { get; set; }
+        public EngineerExperience ComplexityLevel { get; set; }
+    }
+}
