78d0a710f6085c49101257c63549355c6a650333
Raizy-Gutman <rg4183126@gmail.com>
2024-03-27 22:41:14 +0200

    Stage 5 Beginning of Chapter 6

--- PL/Engineer/EngineerListWindow.xaml.cs
+++ PL/Engineer/EngineerListWindow.xaml.cs
@@ -20,6 +20,7 @@ namespace PL.Engineer
     public partial class EngineerListWindow : Window
     {
         static readonly BlApi.IBl s_bl = BlApi.Factory.Get();
+        public BO.EngineerExperience Level { get; set; } = BO.EngineerExperience.None;
         public EngineerListWindow()
         {
             InitializeComponent();
@@ -34,5 +35,10 @@ namespace PL.Engineer
 
         public static readonly DependencyProperty EngineerListProperty =
             DependencyProperty.Register("EngineerList", typeof(IEnumerable<BO.EngineerInList>), typeof(EngineerListWindow), new PropertyMetadata(null));
+
+        private void ComboBox_ChangeSort(object sender, SelectionChangedEventArgs e)
+        {
+            BL.EngineerList.Sort();
+        }
     }
 }
